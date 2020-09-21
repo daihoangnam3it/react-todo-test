63ddf4e164937b99ab2ec4b67777fb86d3438013
Hoàng Đại <daihoangnam3it@gmail.com>
2020-09-21 09:34:50 +0700

    update

--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import uuid from "uuid"
 import "bootstrap/dist/css/bootstrap.min.css"
 export default class TodoInput extends Component {
   render() {
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import uuid from "uuid"
 import "bootstrap/dist/css/bootstrap.min.css"
 import TodoItem from "./TodoItem"
 export default class TodoList extends Component {
