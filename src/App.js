74e38a048df8e510dce79a8a26e3b273b7e0d9ea
Hoàng Đại <daihoangnam3it@gmail.com>
2020-09-21 09:20:49 +0700

    update

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import uuid from 'uuid';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import TodoList from './components/TodoList';
 import TodoInput from './components/TodoInput';
-function App() {
-  return (
-    <div className='container'>
-      <div className='row'>
-        <TodoInput />
-        <TodoList />
+//showing vs-code 
+class App extends React.Component {
+  render(){
+    return (
+      <div className='container'>
+        <div className='row'>
+          <TodoInput />
+          <TodoList />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
