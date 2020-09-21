f052cabd9beda862900d936a7d7e67f8283ba34d
Hoàng Đại <daihoangnam3it@gmail.com>
2020-09-21 09:46:29 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import {v4 as uuidv4} from 'uuid';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import TodoList from './components/TodoList';
 import TodoInput from './components/TodoInput';
 //showing vs-code 
 class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      item:[{id:1,title:"Wake up"}],
+      id:uuidv4(),
+    }
+  }
   render(){
+    console.log(this.state)
     return (
       <div className='container'>
         <div className='row'>
